6f9a2c8cd700e71ccfb1bdebe8da7e43b185e735
Carsten Becker <carbeck+github@gmail.com>
2013-11-28 13:53:28 +0100

    Initial commit of the files proper

--- README.md
+++ README.md
@@ -1,4 +1,90 @@
-wharrgarbl
-==========
+WHARRGARBL.PY
+=============
 
 Generate random pseudo-words based on weighted probabilities with a little Python script.
+
+Executing the Script
+--------------------
+
+You'll need Python – I used version 2.7.3 to make this. Note that this is a *command-line program*. `python wordgen.py -h` gives you some advice on the input:
+
+    positional arguments:
+      <start_string>        The initial rule to kick off the generator, e.g. "{W}"
+                            if your topmost rule is called "W".
+    
+    optional arguments:
+      -h, --help            show this help message and exit
+      -n [N], --number [N]  The number of pseudo-words to be generated. If no
+                            value is given, 10 will be assumed.
+      -r <file>, --rules <file>
+                            File containing the generation rules.
+      -o <file>, --out <file>
+                            File to save the generated list of words in (optional)
+
+Rule files
+----------
+
+Rule files are plain text files and should look like this:
+
+    "W":(
+        ("{C}{V}{C}{V}", 0.5),
+        ("{C}{C}{V}{C}", 0.25),
+        ("{C}{V}{V}", 0.25),
+    ),
+    
+    "C":(
+        ("p", 0.1),
+        ("t", 0.16),
+        ("k", 0.12),
+        ("b", 0.04),
+        ("m", 0.2),
+        ("n", 0.16),
+        ("ng", 0.12),
+        ("v", 0.1)
+    ),
+    
+    "V":(
+        ("i", 0.18),
+        ("e", 0.1),
+        ("a", 0.2),
+        ("o", 0.08),
+        ("u", 0.04),
+        ("{X}", 0.4)
+    ),
+    
+    "X":(
+        ("ay", 0.5),
+        ("ey", 0.5)
+    )
+
+In more general terms, a rule has the following form:
+
+    "rule_name":(
+        ("element", <probability>)
+    )
+    
+Inside an ("element", <probability>) couple, "element" can contain the name
+of a rule itself again:
+
+    ("{rule_name}", <probability>)
+    
+Mixing rule names with non-rules is no problem:
+
+    "X":(
+        ("a{L}a", 0.25),
+        ("a{F}a", 0.75)
+    ),
+    "L":(
+        ("r", 0.5),
+        ("l", 0.5)
+    ),
+    "F":(
+        ("s", 0.5),
+        ("f", 0.25),
+        ("x", 0.25)
+    )
+
+Disclaimer
+----------
+
+This software comes "as-is" with no warranties expressed or implied.
---
+++ wharrgarbl.py
@@ -0,0 +1,99 @@
+#! /usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+''' wharrgarbl.py -- Yet another word generator
+
+    Generate random pseudo-words based on weighted probabilities.
+'''
+
+# Copyleft 2013 Carsten Becker <carbeck@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import argparse
+import ast
+import os
+import os.path
+import random
+import re
+import sys
+
+rulelist = {}
+
+# Pick element from the rule list based on its associated probability
+# rulelist = [(item_0, p_item_0), (item_1, p_item_1), . . ., (item_n, p_item_n)]
+
+def wg_pick(rulelist):
+    p = random.uniform(0,1)
+    p_cum = 0.0
+    for item, p_item in rulelist:
+        p_cum += p_item
+        if p < p_cum:
+            break
+    return item
+    
+
+# Takes matches and replaces them with a randomly picked rule from the rule list
+
+def wg_replacerule(matchobj):
+    string = matchobj.group(1)
+    return wg_pick(rulelist[string])
+
+
+# Recursively walk through the rule list
+
+def wg_rules(string):
+    if bool(re.search(r"{.*?}", string)) == True:
+        string = re.sub(r"{(.*?)}", wg_replacerule, string)
+        return wg_rules(string)
+    else:
+        return string
+
+
+# Main
+
+def main(argv=None):
+    global rulelist
+    if argv is None:
+        argv = sys.argv[1:]
+    
+    parser = argparse.ArgumentParser(prog="wordgen.py", description=
+                "Generate random pseudo-words based on weighted probabilities.")
+    parser.add_argument('-n', '--number', nargs='?', default='10', type=int, 
+            help="The number of pseudo-words to be generated. If no value is "
+            "given, 10 will be assumed.", metavar='N')
+    parser.add_argument('-r', '--rules', dest='rulelist', required=True, help="File containing the generation rules.", metavar='<file>')
+    parser.add_argument('-o', '--out', dest='fileout', metavar='<file>',
+            help="File to save the generated list of words in (optional)")
+    parser.add_argument('start', type=str, metavar='<start_string>',
+            help="The initial rule to kick off the generator, "
+            'e.g. "{W}" if your topmost rule is called "W".')
+    
+    args = parser.parse_args(argv)
+    
+    with open(os.path.join(args.rulelist), mode="r") as rules:
+        rulelist = ast.literal_eval("{" + rules.read() + "}")
+    
+    words = ''
+    for i in xrange(args.n):
+        words += "{}\n".format(wg_rules(args.start))
+    
+    if bool(args.fileout) == True:
+        with open(os.path.join(args.fileout), mode="w") as out:
+            out.write("{}".format(words))
+    else:
+        return words
+
+if __name__ == '__main__':
+    sys.exit(main())

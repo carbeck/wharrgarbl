c7f5af63b09327f85fe7307e67fd4eb168381f17
Carsten Becker <carbeck+github@gmail.com>
2013-12-03 16:57:16 +0100

    Some more formatting and annotation

--- wharrgarbl.py
+++ wharrgarbl.py
@@ -3,7 +3,7 @@
 
 ''' wharrgarbl.py -- Yet another word generator
 
-    Generate random pseudo-words based on weighted probabilities.
+    Generate (pseudo-)random pseudo-words based on weighted probabilities.
 '''
 
 # Copyleft 2013 Carsten Becker <carbeck@gmail.com>
@@ -30,7 +30,7 @@ import re
 import sys
 
 # Pick element from the rule list based on its associated probability
-# rulelist = [(item_0, p_item_0), (item_1, p_item_1), . . ., (item_n, p_item_n)]
+# rulelist = ((item_0, p_item_0), (item_1, p_item_1), ..., (item_n, p_item_n))
 
 def wg_pick(rulelist):
     p = random.uniform(0,1)
@@ -39,14 +39,12 @@ def wg_pick(rulelist):
         p_cum += p_item
         if p < p_cum:
             return item
-    
 
 # Takes matches and replaces them with a randomly picked rule from the rule list
 
 def wg_replacerule(matchobj):
     string = matchobj.group(1)
     return wg_pick(rulelist[string])
-
 
 # Recursively walk through the rule list
 
@@ -57,7 +55,6 @@ def wg_rules(string):
     else:
         return string
 
-
 # Main
 
 def main(argv=None):
@@ -65,12 +62,15 @@ def main(argv=None):
     if argv is None:
         argv = sys.argv[1:]
     
+    # Construct parser and help texts for command-line arguments
+    
     parser = argparse.ArgumentParser(prog="wordgen.py", description=
                 "Generate random pseudo-words based on weighted probabilities.")
     parser.add_argument('-n', '--number', nargs='?', default='10', type=int, 
             help="The number of pseudo-words to be generated. If no value is "
             "given, 10 will be assumed.", metavar='N')
-    parser.add_argument('-r', '--rules', dest='rulelist', required=True, help="File containing the generation rules.", metavar='<file>')
+    parser.add_argument('-r', '--rules', dest='rulelist', required=True, 
+            help="File containing the generation rules.", metavar='<file>')
     parser.add_argument('-o', '--out', dest='fileout', metavar='<file>',
             help="File to save the generated list of words in (optional)")
     parser.add_argument('start', type=str, metavar='<start_string>',
@@ -79,19 +79,24 @@ def main(argv=None):
     
     args = parser.parse_args(argv)
     
+    # Read input file; convert input into a list
+    
     with open(r'{}'.format(args.rulelist), mode="r") as rules:
         rulelist = ast.literal_eval("{" + rules.read() + "}")
+    
+    # Generate number of words provided by command line
     
     words = ''
     for i in xrange(args.number):
         words += "{}\n".format(wg_rules(args.start))
+    
+    # If output is specified as a file, save to file, else print to shell
     
     if bool(args.fileout) == True:
         with open(r'{}'.format(args.fileout), mode="w") as out:
             out.write("{}".format(words))
     else:
         return words
-
 
 if __name__ == '__main__':
     sys.exit(main())

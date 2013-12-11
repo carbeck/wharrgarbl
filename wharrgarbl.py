9eca25578da4eba5c20d37161e70069377c7dc36
Carsten Becker <carbeck+github@gmail.com>
2013-12-11 19:20:11 +0100

    Python script now also contains docstrings as per PEP 257.

--- wharrgarbl.py
+++ wharrgarbl.py
@@ -32,8 +32,8 @@ import sys
 
 # Pick element from the rule list based on its associated probability
 # rulelist = ((item_0, p_item_0), (item_1, p_item_1), ..., (item_n, p_item_n))
-
 def wg_pick(rulelist):
+    """Randomly pick an element from a list based on the element's probability."""
     p = random.uniform(0,1)
     p_cum = 0.0
     for item, p_item in rulelist:
@@ -42,14 +42,14 @@ def wg_pick(rulelist):
             return item
 
 # Takes matches and replaces them with a randomly picked rule from the rule list
-
 def wg_replacerule(matchobj):
+    """Replace the match with a randomly picked element from the rule definitions."""
     string = matchobj.group(1)
     return wg_pick(rulelist[string])
 
 # Recursively walk through the rule list
-
 def wg_rules(string):
+    """As long as rule variables can be found, search through the string."""
     if bool(re.search(r"{.*?}", string)) == True:
         string = re.sub(r"{(.*?)}", wg_replacerule, string)
         return wg_rules(string)
@@ -57,16 +57,15 @@ def wg_rules(string):
         return string
 
 # Main
-
 def main(argv=None):
+    """Main function providing command line option parser etc."""
     global rulelist
     if argv is None:
         argv = sys.argv[1:]
     
     # Construct parser and help texts for command-line arguments
-    
     parser = argparse.ArgumentParser(prog="wharrgarbl.py", description=
-                "Generate random pseudo-words based on weighted probabilities.")
+                "Generate (pseudo-)random pseudo-words based on weighted probabilities.")
     parser.add_argument('-n', '--number', nargs='?', default='10', type=int, 
             help="The number of pseudo-words to be generated. If no value is "
             "given, 10 will be assumed.", metavar='N')
@@ -81,18 +80,15 @@ def main(argv=None):
     args = parser.parse_args(argv)
     
     # Read input file; convert input into a list
-    
     with codecs.open(args.rulelist, mode="r") as rules:
         rulelist = ast.literal_eval("{" + rules.read() + "}")
     
     # Generate number of words provided by command line
-    
     words = ''
     for i in xrange(args.number):
         words += "{}\n".format(wg_rules(args.start))
     
     # If output is specified as a file, save to file, else print to shell
-    
     if bool(args.fileout) == True:
         with codecs.open(args.fileout, mode="w") as out:
             out.write("{}".format(words))
